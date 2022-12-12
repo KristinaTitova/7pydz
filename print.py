590035cfcb8f0aeb305607bf7dcbf6300ccbbf05
KristinaTitova <kristytitova@mail.ru>
2022-12-12 22:06:15 +0300

    принт

--- print.py
+++ print.py
@@ -1,4 +1,4 @@
-def print_data(data):
+def print (data):
     if len(data) > 0:
         print("Фамилия".center(20), "Имя".center(20), "Отчество".center(20), "Телефон".center(15), "Категория".center(30))
         print("-"*130)
