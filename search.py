929966266083dd3c6636b6dc762640e666d1bf85
KristinaTitova <kristytitova@mail.ru>
2022-12-12 22:03:11 +0300

    поиск контакта

---
+++ search.py
@@ -0,0 +1,10 @@
+from export import export
+from print import print
+
+def search (word, data):
+    if len(data) > 0:
+        for item in data:
+            if word in item:
+                return item
+    else:
+        return None
